dfab53cc5f266d660a53cb06e4a6e71a5002300e
zubair <zubairshahzadarain@gmail.com>
2024-11-16 13:36:13 +0000

    Initial commit

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "mysql+mysqlconnector://root:testopenai123@localhost:3306/task_db")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "mysql+mysqlconnector://root:testopenai123@mysqldb:3306/task_db")
 settings = Settings()
